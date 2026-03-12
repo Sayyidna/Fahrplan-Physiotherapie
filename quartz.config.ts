abba9adb7305e65ff7e95c14c57c1b0e20ffda5e
Amadeus Kaiser <amadeuskaiser@posteo.de>
2026-03-12 17:44:06 +0100

    Quartz sync: Mar 12, 2026, 5:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Fahrplan Physiotherapie",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
